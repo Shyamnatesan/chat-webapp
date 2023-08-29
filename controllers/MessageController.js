a67a29402feffc8ea2c1df02c0c2e95e14f7048d
Shyam Natesan <shyamnatesan21@gmail.com>
2023-08-29 16:00:22 +0530

    messageController refactor

--- controllers/MessageController.js
+++ controllers/MessageController.js
@@ -1,11 +1,8 @@
 const Messages = require("../models/Messages");
 
-module.exports.GetMessagesByRoomId = async (req, res) => {
-  console.log("received the request");
+module.exports.GetMessagesByRoomId = async (req, res, next) => {
   const roomId = req.params.roomId;
   const lastMessageTimestamp = req.query.lastMessageTimestamp;
-  console.log("this is the roomID", roomId);
-  console.log(lastMessageTimestamp);
 
   try {
     let query = { roomId };
@@ -16,18 +13,13 @@ module.exports.GetMessagesByRoomId = async (req, res) => {
       };
     }
 
-    console.log(query);
-
     const messagesInRoom = await Messages.find(query)
       .sort({ timestamp: -1 })
       .limit(20);
 
-    console.log(messagesInRoom);
     res.status(200).json({ status: true, messages: messagesInRoom });
   } catch (error) {
     console.log(error);
-    res
-      .status(500)
-      .json({ status: false, error: "error occured while fetching messages" });
+    next(error);
   }
 };
